9b0954a2f0a9fddbb2ae70b07716ee3359783eb4
Dan <drosen@extole.com>
2022-06-30 11:10:04 -0500

    gtm

--- tag-test/src/index.js
+++ tag-test/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module'
 
+const tagManagerArgs = {
+  gtmId: 'GTM-XXXXXX'
+}
+TagManager.initialize(tagManagerArgs)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
